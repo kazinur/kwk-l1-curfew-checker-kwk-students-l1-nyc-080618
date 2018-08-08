41567e850c8e3b4fd546f647e12d5c08955ff0ea
kazinur <kazinur@mybihs.org>
2018-08-08 18:44:57 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,13 +1,20 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if 6<7
+    puts "six is less than seven"
 end
 
 def curfew_checker(time)
-  # code goes here
+  if 5>4
+    puts "five is greater than four"
+  else
+    puts "five is not greater than four"
 end
 
 def complex_curfew_checker(time)
-  # code goes here
+ if 9>11 
+   puts "nine is greater than eleven "
+ elsif
+ puts ""
 end
 
 def deluxe_curfew_checker(time)
